e2e079fd13f85678ec9de7695c9ab86ec64db2b6
anam08 <anam@fkipunlam.ac.id>
2018-03-14 11:30:22 +0800

    Add files via upload

---
+++ coin.js
@@ -0,0 +1,13 @@
+var pair = {
+    'XTL-BTC' : 'crex24',
+    'CREP-BTC' : 'crex24',
+    'BBS-BTC' : 'crex24',
+    'BTC-USD' : 'crex24'
+}
+
+module.exports = {
+    Pair : function()
+    {
+        return pair;
+    }
+}
---
+++ init.js
@@ -0,0 +1,66 @@
+var http = require('http');
+var https = require('https');
+var url = require('url');
+var allowedOrigin = 'npool.pw';
+const port = process.env.PORT || 9026;
+
+var coin = require('./coin.js');
+var pair = coin.Pair();
+
+var server = http.createServer(function(request, response)
+{
+    if(typeof(request.headers.origin)!='undefined')
+    {
+        if(request.headers.origin.indexOf(allowedOrigin)==-1)
+        {
+            response.writeHead("204", "No Content", {
+                "access-control-allow-origin": '*',
+                "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+                "access-control-allow-headers": "content-type, accept",
+                "access-control-max-age": 10, // Seconds.
+                "content-length": 0
+            });
+            
+            return(response.end());
+        }
+    }
+    
+    if (request.method.toUpperCase() === "OPTIONS")
+    {
+        response.writeHead("204", "No Content", {
+            "access-control-allow-origin": '*',
+            "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+            "access-control-allow-headers": "content-type, accept",
+            "access-control-max-age": 10, // Seconds.
+            "content-length": 0
+        });
+        
+        return(response.end());
+    }
+    
+    var urlParts = url.parse(request.url, true);
+    var pairPart = (urlParts.pathname).replace('/', '');
+    if(typeof(pair[pairPart])!='undefined')
+    {
+        var func = 'handle'+pair[pairPart];
+        var market = require('./lib/market.js');
+        market[func](pairPart, response);
+    }
+    else
+    {
+        response.writeHead(404, {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json'
+        });
+        response.end(JSON.stringify({error: 'Invalid pair call'}));
+    }
+});
+
+server.listen(port, function(err){
+    if (err)
+    {
+        return console.log('something bad happened', err);
+    }
+    
+    console.log('server is listening on '+port);
+});
