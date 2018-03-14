76356eb5515c2d1ed1c84806703997b67c02d77d
anam08 <anam@fkipunlam.ac.id>
2018-03-14 14:27:46 +0800

    Update coin.js

--- coin.js
+++ coin.js
@@ -2,7 +2,8 @@ var pair = {
     'XTL-BTC' : 'crex24',
     'CREP-BTC' : 'crex24',
     'BBS-BTC' : 'crex24',
-    'BTC-USD' : 'crex24'
+    'BTC-USD' : 'crex24',
+    'IPBC-BTC' : 'crex24'
 }
 
 module.exports = {
